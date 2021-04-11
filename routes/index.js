187ad457a8b205e73f4ed1da7dea9bc6f9fa30bb
Jelena Feliciano <jfel217@gmail.com>
2021-04-10 20:44:56 -0400

    rendered the recipe data to the index.pug file Cinnamon images aren't loading though

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const { recipes } = require('../data/data.json');
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // 1. Pass all recipe data to 'index' template
-  res.render('index', () => {
-    res.send(recipes);
-  });
+  res.render('index', {recipes});
 });
 
 /* GET recipe page. */
 router.get('/recipes/:id', function(req, res, next) {
+  //stores the value of the id route parameter
   const recipeId = req.params.id;
+  //holds recipe object to pass into the view 
   const recipe = recipes.find( ({ id }) => id === +recipeId );
-  
+  //render recipe info available
   if (recipe) {
     // 2. Pass the recipe data to the 'recipe' template
-    res.render('recipe', ()=> {
-      //res.send();
-    });
+    res.render('recipe', {recipe});
   } else {
     res.sendStatus(404);
   }
